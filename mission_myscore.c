4f8588cd94208166ef2db1b78216d4cc1a129168
soom1n <128569645+soom1n@users.noreply.github.com>
2023-05-16 06:25:36 +0000

    조건문 끝

---
+++ mission_myscore.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// main() {
+//   int x;
+//   printf("점수를 입력하시오.");
+//   scanf("%d", &x);
+
+//   if (x >= 90)
+//     printf(" A학점 ");
+//   else if ( x>= 80)
+//     printf("B학점");
+//   else if ( x>= 70)
+//     printf("C학점");
+//   else if ( x>=60)
+//     printf ("D학점");
+//   else
+//     printf("F학점");
+// }
